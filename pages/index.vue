3e3d85cb3b1a45543d35ee3eb967e4fa6e153efb
hacklover <info@hacklover.net>
2020-09-30 05:32:25 +0200

    Add tooltip details near demo button

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <v-row justify="center" align="center" v-if="!demoActive">
-    <v-col cols="12" class="text-center">
+    <v-col cols="12" class="owd-intro text-center">
       <Logo style="margin: 0 auto" />
 
       <p class="mt-8">
@@ -8,6 +8,13 @@
       </p>
       <p class="mb-0">
         <a @click="toggleDemo">Check the demo!</a>
+
+        <v-tooltip bottom attach=".owd-intro">
+          <template v-slot:activator="{ on, attrs }">
+            <v-icon v-on="on">mdi-information-variant</v-icon>
+          </template>
+          <span>This demo is my personal website and it's based on Open Web Desktop.<br />Check <b>owd-client</b> on GitHub if you are looking for a fresh demo!</span>
+        </v-tooltip>
       </p>
     </v-col>
   </v-row>
@@ -59,6 +66,35 @@ export default {
 </script>
 
 <style scoped lang="scss">
+  .owd-intro {
+    p {
+      .v-icon {
+        margin-left: 7.5px;
+        vertical-align: -3px;
+        opacity: 0.25;
+      }
+    }
+
+    .v-tooltip__content {
+      position: absolute !important;
+      left: 0 !important;
+      right: 0 !important;
+      min-width: 480px;
+      margin-top: 10px;
+      margin-left: 50%;
+      transform: translateX(-50%);
+      padding: 12px;
+
+      @media(max-width: 560px) {
+        min-width: 80vw;
+
+        br {
+          display: none;
+        }
+      }
+    }
+  }
+
   iframe {
     border: 0;
     position: absolute;
